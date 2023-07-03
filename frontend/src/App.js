1924cd3de2002cf3701038b63d3c37a3261de5b3
hannadarwish <hannadarwish@gmail.com>
2023-07-02 19:22:44 -0700

    frontend complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ import NavBar from './components/NavBar/NavBar';
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
+import Tweets from './components/Tweets/Tweets';
+import Profile from './components/Profile/Profile';
+import TweetCompose from './components/Tweets/TweetCompose';
 
 import { getCurrentUser } from './store/session';
 
@@ -27,7 +30,13 @@ function App() {
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
+
+        <ProtectedRoute exact path="/tweets" component={Tweets} />
+        <ProtectedRoute exact path="/profile" component={Profile} />
+        <ProtectedRoute exact path="/tweets/new" component={TweetCompose} />
       </Switch>
     </>
   );
 }
+
+export default App;
